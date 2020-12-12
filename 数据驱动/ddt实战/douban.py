038cac28fa30db6afa96f1eafb3d6e89f7c0e6b3
227690 <yang_wenbo@dahuatech.com>
2020-12-12 13:49:44 +0800

    UI自动化内容更新

---
+++ 数据驱动/ddt实战/douban.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import unittest
+from selenium import webdriver
+from ddt import data,unpack,ddt
+
+
+@ddt
+class DoubanLogin(unittest.TestCase):
+
+    def setUp(self):
+        print('开始登陆')
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get('https://www.douban.com/')
+        self.driver.implicitly_wait(30)
+
+    def tearDown(self):
+        self.driver.quit()
+        print('结束登陆')
+
+
+    """
+    @data表示元组的列表数据
+    @unpack表示用来解压元组到多个参数
+    """
+    @data(('123456789','122','手机格式不正确'),('18721706546','12','输入的密码不正确'))
+    @unpack
+    def test_login(self,tel,password,result):
+        pwd_login = self.driver.find_element_by_class_name('account-tab-account')
+        pwd_login.click()
+        self.driver.find_element_by_id('username').send_keys(tel)
+        self.driver.find_element_by_id('password').send_keys(password)
+        login_button = self.driver.find_element_by_class_name('btn btn-account')
+        login_button.click()
+        actual_result = self.driver.find_element_by_class_name('fatal-msg hide').text
+        self.assertEqual(actual_result,result)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
